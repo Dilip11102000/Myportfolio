75f2ae3080ed5636f7f16b7c1147c2f1abec1940
Dilip <dilip@cdp360.com>
2024-07-25 11:58:54 +0530

    new file

---
+++ src/commoncontent/ContactData.js
@@ -0,0 +1,24 @@
+export const ContactDatas = {
+    conntactList: [
+        {
+            id: 1,
+            name: "Chennai",
+            image: <i class="fa-solid fa-location-dot icon-color"></i>
+        },
+        {
+            id: 2,
+            name: "+91 6369330231",
+            image: <i class="fa-solid fa-phone icon-color"></i>
+        },
+        {
+            id: 3,
+            name: "dilipgovindhan2000@gmail.com",
+            image: <i class="fa-solid fa-envelope icon-color"></i>
+        }
+    ],
+    name: "Name",
+    email: "Email",
+    message: "Messag",
+    help: "Let’s talk how I can help you!",
+    getintouch:"Get in touch"
+}
