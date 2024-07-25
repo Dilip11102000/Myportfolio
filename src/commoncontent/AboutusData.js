a4d1933af646111c62d509e5bdd2db4f953ea655
Dilip <dilip@cdp360.com>
2024-07-25 17:51:19 +0530

    page login

--- src/commoncontent/AboutusData.js
+++ src/commoncontent/AboutusData.js
@@ -1,143 +1,138 @@
 export const TabContent = [
-    {
-        id: 1,
-        name: "Personal Info"
-    },
-    {
-        id: 2,
-        name: "Qualifications"
-    },
-    {
-        id: 3,
-        name: "Skills"
-    }
-]
-
+  {
+    id: 1,
+    name: "Personal Info",
+  },
+  {
+    id: 2,
+    name: "Qualifications",
+  },
+  {
+    id: 3,
+    name: "Skills",
+  },
+];
 
 export const PersonalContent = {
-    name: "Dilip.G",
-    phoneno: "+91 6369330231",
-    emailid: "dilipgovindhan2000@gmail.com",
-    dob: "Born on 11 July,2000",
-    deggre: "Master of Business Administration,(operation management & system)(MBA)",
-    address: "chennai",
-    desc: "I specialize in crafting intuitive websites with cutting-edge techonogy, delivering dynamic and enaging user experiences.",
-    languageName: "Language Skill",
-    MyAwesomeJourney: 'My Awesome Journey',
-    tools: "Tools I Use Everyday",
-    languages: [
-        {
-            id: 1,
-            name: "English"
-        },
-        {
-            id: 2,
-            name: "Tamil"
-        },
-        {
-            id: 3,
-            name: "Telugu"
-        },
-       
-    ]
-}
-
+  name: "Dilip.G",
+  phoneno: "+91 6369330231",
+  emailid: "dilipgovindhan2000@gmail.com",
+  dob: "Born on 11 July,2000",
+  deggre:
+    "Master of Business Administration,(operation management & system)(MBA)",
+  address: "chennai",
+  desc: "I specialize in crafting intuitive websites with cutting-edge techonogy, delivering dynamic and enaging user experiences.",
+  languageName: "Language Skill",
+  MyAwesomeJourney: "My Awesome Journey",
+  tools: "Tools I Use Everyday",
+  languages: [
+    {
+      id: 1,
+      name: "English",
+    },
+    {
+      id: 2,
+      name: "Tamil",
+    },
+    {
+      id: 3,
+      name: "Telugu",
+    },
+  ],
+};
 
 export const Qualifications = [
-    {
-        id: 1,
-        name: "Bachelor of Business Administration",
-        dgname: "( BBA )",
-        clgName: "Sri Vidya Mandir Arts & Science College",
-        year: "2017-2020",
-        place: "Krishnagar",
-        calicon: <i class="fa-regular fa-calendar cal-icon"></i>
-    },
-    {
-        id: 2,
-        name: "Master of Business Administration (Operation Management & System)",
-        dgname: "( MBA )",
-        clgName: "Excel Engineering College",
-        year: "2020 - 2022,",
-        place: "Namakkal,",
-        calicon: <i class="fa-regular fa-calendar cal-icon"></i>
-    },
-   
-    {
-        id: 3,
-        name: "Higher Secondary School Certificate ",
-        dgname: "( HSC )",
-        year: "2015 - 2017",
-        clgName: "Government Boys Hr Sec School ",
-        place: "Krishnagar",
-        calicon: <i class="fa-regular fa-calendar cal-icon"></i>
+  {
+    id: 1,
+    name: "Bachelor of Business Administration",
+    dgname: "( BBA )",
+    clgName: "Sri Vidya Mandir Arts & Science College",
+    year: "2017-2020",
+    place: "Krishnagar",
+    calicon: <i class="fa-regular fa-calendar cal-icon"></i>,
+  },
+  {
+    id: 2,
+    name: "Master of Business Administration (Operation Management & System)",
+    dgname: "( MBA )",
+    clgName: "Excel Engineering College",
+    year: "2020 - 2022,",
+    place: "Namakkal,",
+    calicon: <i class="fa-regular fa-calendar cal-icon"></i>,
+  },
 
-    },
-    {
-        id: 4,
-        name: "Secondary School Leaving Certificate",
-        dgname: "( SSLC )",
-        year: "2015",
-        clgName: "Government Boys Hr Sec School",
-        place: "Krishnagar",
-        calicon: <i class="fa-regular fa-calendar cal-icon"></i>
-
-    }
-]
-
+  {
+    id: 3,
+    name: "Higher Secondary School Certificate ",
+    dgname: "( HSC )",
+    year: "2015 - 2017",
+    clgName: "Government Boys Hr Sec School ",
+    place: "Krishnagar",
+    calicon: <i class="fa-regular fa-calendar cal-icon"></i>,
+  },
+  {
+    id: 4,
+    name: "Secondary School Leaving Certificate",
+    dgname: "( SSLC )",
+    year: "2015",
+    clgName: "Government Boys Hr Sec School",
+    place: "Krishnagar",
+    calicon: <i class="fa-regular fa-calendar cal-icon"></i>,
+  },
+];
 
 export const Skills = [
-    {
-        id: 1,
-        name: "Reactjs"
-    },
-   
-   
-   
-    {
-        id: 3,
-        name: "Context Api"
-    },
-    {
-        id: 4,
-        name: "Redux"
-    },
-    {
-        id: 5,
-        name: "Javascript"
-    },
-    {
-        id: 6,
-        name: "HTML"
-    },
-    {
-        id: 7,
-        name: "CSS"
-    },
-    {
-        id: 8,
-        name: "SASS"
-    },
-    {
-        id: 9,
-        name: "UI/UX"
-    },
-    {
-        id: 10,
-        name: "Bootstrap"
-    },
-    {
-        id: 11,
-        name: "Github"
-    },
-    {
-        id: 12,
-        name: "Gitlab"
-    },
-    {
-        id: 13,
-        name: "Responsive UI Designs"
-    },
+  {
+    id: 1,
+    name: "Reactjs",
+  },
+  {
+    id: 2,
+    name: "ReactNative",
+  },
 
-
-]
+  {
+    id: 3,
+    name: "Context Api",
+  },
+  {
+    id: 4,
+    name: "Redux",
+  },
+  {
+    id: 5,
+    name: "Javascript",
+  },
+  {
+    id: 6,
+    name: "HTML",
+  },
+  {
+    id: 7,
+    name: "CSS",
+  },
+  {
+    id: 8,
+    name: "SASS",
+  },
+  {
+    id: 9,
+    name: "UI/UX",
+  },
+  {
+    id: 10,
+    name: "Bootstrap",
+  },
+  {
+    id: 11,
+    name: "Github",
+  },
+  {
+    id: 12,
+    name: "Gitlab",
+  },
+  {
+    id: 13,
+    name: "Responsive UI Designs",
+  },
+];
